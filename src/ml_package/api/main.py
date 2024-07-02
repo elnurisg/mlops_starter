eee2a167a6c20207f0a7f173440df68d0f1afb0c
Elnur Isgandarov <elnur1isgenderov@gmail.com>
2024-07-02 16:38:57 +0200

    Load model with startup in lifespan and fix related bugs

--- src/ml_package/api/main.py
+++ src/ml_package/api/main.py
@@ -5,24 +5,25 @@ from typing import List
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, conlist
 from contextlib import asynccontextmanager
+from fastapi.testclient import TestClient
 
 from src.ml_package.models.logistic_regression import load_model
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-app = FastAPI()
 model = None
 
 # Loading the model on startup would help in avoiding the overhead of loading the model for every request
 @asynccontextmanager
-async def lifespan(app: FastAPI):
+async def app_lifespan(app: FastAPI):
     global model
     model = load_model()
     if model is None:
         raise HTTPException(status_code=500, detail="Model not found")
+    yield
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(lifespan=app_lifespan)
 
 # Defining the request body corresponding the data
 class PredictRequest(BaseModel):
@@ -30,10 +31,11 @@ class PredictRequest(BaseModel):
     
 @app.post("/predict") # Model inference is a CPU-bound operation.
 def predict(request: PredictRequest):
-    if not model:
-        raise HTTPException(status_code=500, detail="Model is not loaded")
-    prediction = model.predict([request.data])
-    return {"prediction": prediction.tolist()}
+    with TestClient(app) as client:
+        if not model:
+            raise HTTPException(status_code=500, detail="Model is not loaded")
+        prediction = model.predict([request.data])
+        return {"prediction": prediction.tolist()}
 
 @app.get("/")
 def read_root():
